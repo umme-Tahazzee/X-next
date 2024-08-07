658897ec41036910b6135b376412ffbfe93341e6
umme-Tahazzee <tahazzeekaria1@gmail.com>
2024-08-07 12:38:16 -0700

    update layout and news component

--- src/app/layout.js
+++ src/app/layout.js
@@ -15,13 +15,20 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         <div className="flex justify-between max-w-6xl mx-auto">
-          <div className='hidden sm:inline border-r h-screen '>
-            <Sidebar  />
+          <div className="hidden sm:inline border-r h-screen ">
+            <Sidebar />
           </div>
           <div>{children}</div>
-          <div>
-            {" "}
-            <News />{" "}
+          <div className="lg:flex-col p-3 h-screen border-l hidden lg:flex w-[24rem]">
+            <div className="sticky top-0  bg-white py-2">
+              <input
+                type="text"
+                placeholder="Search"
+                className="bg-gray-100 border-gray-200 
+                   rounded-3xl text-sm w-full px-4 py-2 "
+              ></input>
+            </div>
+            <News />
           </div>
         </div>
       </body>
--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,11 +1,43 @@
-import React from 'react'
+"use client";
+
+import { useEffect, useState } from "react";
 
 const News = () => {
-  return (
-    <div>
-         News 
-    </div>
-  )
-}
+  const [news, setNews] = useState([]);
+  const [articalNum, setArticalNum] = useState(4);
+  useEffect(() => {
+    fetch("https://saurav.tech/NewsAPI/top-headlines/category/business/us.json")
+      .then((res) => res.json())
+      .then((data) => {
+        setNews(data.articles);
+      });
+  }, []);
 
-export default News
+  return (
+    <div className="text-gray-700 space-y-3 bg-gray-100 rounded-xl pt-2 ">
+      <h4 className="font-bold text-xl px-4 ">Whats Happening</h4>
+      {news.slice(0, articalNum).map((article) => (
+        <div key={article.url}>
+          <a href={article.url} target="blank">
+            <div className="flex flex-center justify-between px-4 py-2 space-x-1  hover:bg-gray-200 transition duration-200 ">
+              <div className="space-y-0 ">
+                <h6 className="text-sm font-bold">{article.title}</h6>
+                <p className="text-xs font-medium ">{article.source.name}</p>
+              </div>
+              <img
+                src={article.urlToImage}
+                width={70}
+                className="rounded-xl"
+              />
+            </div>
+          </a>
+        </div>
+      ))}
+      <button
+      onClick={()=> setArticalNum(articalNum+3)}
+      className="text-blue-700 pl-4 pb-3 hover:text-blue-400 text-sm">Load More</button>
+    </div>
+  );
+};
+
+export default News;
